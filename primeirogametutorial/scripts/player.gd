fae2835879a13b1501dd9c3244deed93470dcefc
Mauro Cesar <147224628+maurocesarjunior@users.noreply.github.com>
2025-04-18 14:51:26 -0300

    Add hitbox e hurtbox

---
+++ primeirogametutorial/scripts/player.gd
@@ -0,0 +1,47 @@
+extends CharacterBody2D
+
+
+const SPEED = 200.0
+const JUMP_FORCE = -400.0
+
+@onready var animation := $anim as AnimatedSprite2D
+@onready var remote_transform := $Remote as RemoteTransform2D
+var is_jumping := false
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_FORCE
+		is_jumping = true
+	elif is_on_floor():
+		is_jumping = false
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("esquerda", "direita")
+	if direction:
+		velocity.x = direction * SPEED
+		animation.scale.x = direction
+		if !is_jumping:
+			animation.play("run")
+	elif is_jumping:
+		animation.play("jump")
+		
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		animation.play("idle")
+		
+	move_and_slide()
+
+
+func _on_hurtbox_body_entered(body: Node2D) -> void:
+	if body.is_in_group("enemies"):
+		queue_free()
+		
+func follow_camera(camera):
+	var camera_path = camera.get_path()
+	remote_transform.remote_path = camera_path
