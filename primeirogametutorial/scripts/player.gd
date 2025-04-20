a4fd35025e96809427ce8224602630d4a8e0bc10
Mauro Cesar <147224628+maurocesarjunior@users.noreply.github.com>
2025-04-20 12:25:55 -0300

    Add controls touch

--- primeirogametutorial/scripts/player.gd
+++ primeirogametutorial/scripts/player.gd
@@ -2,7 +2,12 @@ extends CharacterBody2D
 
 
 const SPEED = 200.0
-const JUMP_FORCE = -400.0
+const JUMP_FORCE = -350.0
+var player_life := 10
+var knockback_vector := Vector2.ZERO
+
+@onready var ray_Right := $ray_right as RayCast2D
+@onready var ray_Left := $ray_left as RayCast2D
 
 @onready var animation := $anim as AnimatedSprite2D
 @onready var remote_transform := $Remote as RemoteTransform2D
@@ -34,14 +39,33 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		animation.play("idle")
-		
+	
+	if knockback_vector != Vector2.ZERO:
+		velocity = knockback_vector
 	move_and_slide()
 
 
 func _on_hurtbox_body_entered(body: Node2D) -> void:
-	if body.is_in_group("enemies"):
+	if player_life < 0:
 		queue_free()
+	else:
+		if ray_Right.is_colliding():
+			take_damage(Vector2(-200, -200))
+		elif ray_Left.is_colliding():
+			take_damage(Vector2(200, -200))
 		
 func follow_camera(camera):
 	var camera_path = camera.get_path()
 	remote_transform.remote_path = camera_path
+
+
+func take_damage(knockback_force := Vector2.ZERO, duration := 0.25):
+	player_life -= 1
+	
+	if knockback_force != Vector2.ZERO:
+		knockback_vector = knockback_force
+		
+		var knockback_tween := get_tree().create_tween().set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN_OUT)
+		knockback_tween.parallel().tween_property(self, "knockback_vector", Vector2.ZERO, duration)
+		animation.modulate = Color(1,0,0,1)
+		knockback_tween.parallel().tween_property(animation, "modulate", Color(1,1,1,1), duration)
