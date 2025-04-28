7cf8f9cd7f5009f35d26ed41bf74f91341a150bc
Mauro Cesar <147224628+maurocesarjunior@users.noreply.github.com>
2025-04-28 18:32:25 -0300

    correção text

--- primeirogametutorial/singletons/dialog_manager.gd
+++ primeirogametutorial/singletons/dialog_manager.gd
@@ -1,41 +1,45 @@
 extends Node
 
 @onready var dialog_box_scene = preload("res://prefabs/dialog_box.tscn")
-var message_lines : Array[String] = []
-
+var message_lines: Array[String] = []
 var current_line = 0
-var dialog_box
-var dialog_box_position := Vector2.ZERO
-var is_message_active := false
-var can_advance_message := false
+var dialog_box = null
+var is_message_active = false
+var dialog_box_position := Vector2.ZERO  # posição do diálogo
 
+# Começa o diálogo
 func start_message(position: Vector2, lines: Array[String]):
 	if is_message_active:
 		return
-	
+
 	message_lines = lines
+	current_line = 0
 	dialog_box_position = position
 	show_text()
 	is_message_active = true
-	
-func show_text():
-	dialog_box = dialog_box_scene.instantiate()
-	dialog_box.text_display_finished.connect(_on_all_text_displayed)
-	get_tree().root.add_child(dialog_box)
-	dialog_box.global_position = dialog_box_position
-	
-	dialog_box.display_text(message_lines[current_line])
-	can_advance_message = false
-	
-func _on_all_text_displayed():
-	can_advance_message = true
 
+# Exibe a linha de texto atual
+func show_text():
+	if dialog_box == null:
+		dialog_box = dialog_box_scene.instantiate()
+		get_tree().root.add_child(dialog_box)
+
+	dialog_box.global_position = dialog_box_position
+	dialog_box.display_text(message_lines[current_line])
+
+# Escuta a tecla de "pular diálogo"
 func _unhandled_input(event):
-	if(event.is_action_pressed("advance_message") && is_message_active && can_advance_message):
-		dialog_box.queue_free()
+	if event.is_action_pressed("pular_dialogo") and is_message_active:
 		current_line += 1
 		if current_line >= message_lines.size():
-			is_message_active = false
-			current_line = 0
-			return
-		show_text()
+			close_dialog()
+		else:
+			show_text()
+
+# Fecha o diálogo
+func close_dialog():
+	if dialog_box:
+		dialog_box.queue_free()
+		dialog_box = null
+	is_message_active = false
+	current_line = 0
